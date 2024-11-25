b2342c6e7fad86f869aa9b6c4c2ecaa603fde7e1
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-25 11:20:03 -0600

    Change port to 8000

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -2,4 +2,4 @@ import uvicorn
 
 def dev():
   """ Launched with 'poetry run dev' at root level """
-  uvicorn.run("server.main:app", host="0.0.0.0", port=3000, reload=True)
+  uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
