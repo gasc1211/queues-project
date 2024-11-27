2ad8a062eef5ed03abe59a44b2ec1842cd050876
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-27 09:36:33 -0600

    Add Queue Trigger and VSCode config

--- function/function_app.py
+++ function/function_app.py
@@ -1,6 +1,14 @@
 import azure.functions as func
-import datetime
-import json
+from dotenv import load_dotenv
 import logging
+import os
+
+load_dotenv()
 
 app = func.FunctionApp()
+
+@app.queue_trigger(arg_name="azqueue", queue_name="account-activation",
+                               connection="saaccountingsystemdev_STORAGE") 
+def QueueTriggerFunctionActivateAccount(azqueue: func.QueueMessage):
+    body = azqueue.get_body().decode('utf-8')    
+    logging.info('Python Queue trigger processed a message: %s', body)
