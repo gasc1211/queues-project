9831f5192cb5468fa8639f1da22077235348b815
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-27 20:13:32 -0600

    Add environment vars

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,5 +1,14 @@
+""" Queue Function Backend API """
+
 import uvicorn
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
+
+__host__ = os.getenv("HOST")
+__port__ = int(os.getenv("PORT"))
 
 def dev():
   """ Launched with 'poetry run dev' at root level """
-  uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+  uvicorn.run("server.main:app", host=__host__, port=__port__, reload=True)
