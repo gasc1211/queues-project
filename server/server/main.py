e436fe37c42235387359b6408dc6c319770ba13d
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-28 06:28:58 -0600

    Add base endpoints

--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,28 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+
+from models.UserSignup import UserSignup
+from models.UserLogin import UserLogin
+from controllers.firebase import register_user_firebase, login_user_firebase
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
   return "Hello from FastAPI!"
+
+
+@app.post("/signup")
+async def signup(user: UserSignup):
+  """ Signup for the application """
+  return await register_user_firebase(user)
+
+@app.post("/login")
+async def login(user: UserLogin):
+  """ Login into the application"""
+  return await login_user_firebase(user)
+
+@app.get("/user")
+async def getUser(req: Request, res: Response):
+  """ Get user data"""
+  pass
