42ccb41c54bd85b198f2a29233ba445adef718bb
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-11-28 21:32:22 -0600

    Add email model

---
+++ server/server/models/EmailActivation.py
@@ -0,0 +1,13 @@
+import re
+from pydantic import BaseModel, field_validator
+from typing import Optional
+
+class EmailActivation(BaseModel):
+    email: str
+
+    @field_validator('email')
+    def email_validation(cls, value):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
+            raise ValueError('Invalid email address')
+
+        return value
